e8e30f2f926bf82afffb8b36111f1cfd26e9f8c6
tixons2 <tixons.i.t@gmail.com>
2020-05-12 00:42:33 +0300

    script v1

---
+++ create_setup.py
@@ -0,0 +1,16 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("users", help="specify users ammount", type=int)
+parser.add_argument("db", help="specify db path", type=str)
+args = parser.parse_args()
+
+f = open(args.db, 'r')
+sql_script_data = f.read()
+f.close()
+file = open('./db_and_users.sql', 'w')
+for i in range(1, args.users + 1):
+    file.write(f"CREATE USER 'user_{i}'@'%' IDENTIFIED BY 'user{i}password';\n")
+    file.write(sql_script_data.replace('classicmodels', f'db_user_{i}'))
+    file.write(f"GRANT ALL PRIVILEGES ON db_user_{i}.* TO \'user_{i}\'@\'%\'\n")
+    file.write('FLUSH PRIVILEGES;\n')
+file.close()
